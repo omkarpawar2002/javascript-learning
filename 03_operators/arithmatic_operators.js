baec6c20ce76a33c79300bd68f32f8de5f626310
omkar <omkarsp20@gmail.com>
2026-01-30 19:10:49 +0530

    Learn Arithmatic Operators

---
+++ 03_operators/arithmatic_operators.js
@@ -0,0 +1,23 @@
+/*
+Arithmatic Operators :- 
+        Arithmatic operators are used to perform the arithmatic operations such as addition , subtraction , multiplication and division etc.,
+
+        Arithmatic operators are :- 
+            1.addition
+            2.subtraction
+            3.multiplication
+            4.division
+            5.exponent
+            6.modulo
+
+*/
+
+let num1 = 12;
+let num2 = 3;
+console.log(`Addition of ${num1} + ${num2} :- ${num1 + num2}`);
+console.log(`Subtraction of ${num1} - ${num2} :- ${num1 - num2}`);
+console.log(`Multiplication of ${num1} * ${num2} :- ${num1 * num2}`);
+console.log(`Division of ${num1} / ${num2} :- ${num1 / num2}`);
+console.log(`Modulus of ${num1} % ${num2} :- ${num1 % num2}`);
+console.log(`Exponent of ${num1} ** ${num2} :- ${num1 ** num2}`);
+
