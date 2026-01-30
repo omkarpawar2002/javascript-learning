a242ae2a887e1057ed15b658a7076117893221e6
omkar <omkarsp20@gmail.com>
2026-01-30 19:14:49 +0530

    Learn Ternary Operators

---
+++ 03_operators/ternary_operator.js
@@ -0,0 +1,14 @@
+/*
+Ternary operator :- 
+        Ternary operator evaluates the condition and based on the condition execute the block of code.
+        Ternary operator required 3 operands.
+
+        syntax :- (condition) ? expression 1 : expression 2
+                
+        Here condition is operand1 , expression 1 is operand 2 and expression 2 is operand 3
+
+*/
+
+let age = 19;
+let result = (age >= 18) ? "Eligible for vote" : "Not Eligible for vote";
+console.log(`You are ${result}`);
