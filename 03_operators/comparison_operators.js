50ef581e320c08a994cc05cc0373878b25320329
omkar <omkarsp20@gmail.com>
2026-01-30 19:12:39 +0530

    Learn Comparison Operators

---
+++ 03_operators/comparison_operators.js
@@ -0,0 +1,25 @@
+/*
+Comparison Operators :- 
+            Comparison operators are used to compare two values and return a boolean result.
+
+            Comparison operators are :- 
+                1.equals to 
+                2.not equals to 
+                3.greater than
+                4.greater than equals to 
+                5.less than
+                6.less than equals to
+                7.strict equals to
+                8.not strict equals to
+*/
+
+let num1 = 10;
+let num2 = "3";
+console.log(`${num1} is equals to ${num2} = ${num1 == num2}`);
+console.log(`${num1} is not equals to ${num2} = ${num1 != num2}`);
+console.log(`${num1} is greater than ${num2} = ${num1 > num2}`);
+console.log(`${num1} is greater than or equals to ${num2} = ${num1 >= num2}`);
+console.log(`${num1} is less than ${num2} = ${num1 < num2}`);
+console.log(`${num1} is less than or equals to ${num2} = ${num1 <= num2}`);
+console.log(`${num1} is strictly equals to ${num2} = ${num1 === num2}`);
+console.log(`${num1} is strictly not equals to ${num2} = ${num1 !== num2}`);
