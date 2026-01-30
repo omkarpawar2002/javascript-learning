8be0c79900253b2f7f6610b5db35effa1001e228
omkar <omkarsp20@gmail.com>
2026-01-30 19:15:16 +0530

    Learn TypeOf Operators

---
+++ 03_operators/typeof_operator.js
@@ -0,0 +1,21 @@
+/*
+typeof operator :- 
+        typeof operator is used to check the data type of variable.
+*/
+
+// If we want to check the data type of variable then we used typeOf operator :- 
+
+let first_string = "welcome";
+let first_number = 23;
+let first_boolean = true;
+let first_null = null;
+let first_undefined;
+let first_bigint = 23455321234445n;
+let first_symbol = Symbol();
+console.log(first_string,typeof first_string);
+console.log(first_number,typeof first_number);
+console.log(first_boolean,typeof first_boolean);
+console.log(first_null,typeof first_null);
+console.log(first_undefined,typeof first_undefined);
+console.log(first_bigint,typeof first_bigint);
+console.log(first_symbol,typeof first_symbol);
