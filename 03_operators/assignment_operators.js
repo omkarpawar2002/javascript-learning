7cdec4fbe5fa07f7e1a0d4dbb9a10f7fb5bb29ed
omkar <omkarsp20@gmail.com>
2026-01-30 19:11:52 +0530

    Learn Assignment Operators

---
+++ 03_operators/assignment_operators.js
@@ -0,0 +1,23 @@
+/*
+Assignment Operators :- 
+            Assignment operators are used to assign the right hand side value to the left hand side of variable.
+
+            Assignment operators are :- 
+                1.Addition Assignment
+                2.Subtraction Assignment
+                3.Multiplication Assignment
+                4.Division Assignment
+                5.Modulus Assignment
+                6.Exponent Assignment
+
+            Here we used = for an assignment it assign the right hand side value to the left hand side of variable.
+*/
+
+let num1 = 3;
+console.log(`Value of num1 :- ${num1}`);
+console.log(`Addition Assignment :- ${num1 += 1}`)
+console.log(`Subtraction Assignment :- ${num1 -= 1}`)
+console.log(`Multiplication Assignment :- ${num1 *= 1}`)
+console.log(`Division Assignment :- ${num1 /= 1}`)
+console.log(`Modulus Assignment :- ${num1 %= 1}`)
+console.log(`Exponent Assignment :- ${num1 **= 1}`)
