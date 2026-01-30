aa92c462a7f81f99b9840eaa6388d0fa2fb328d4
omkar <omkarsp20@gmail.com>
2026-01-30 18:49:43 +0530

    Added Variable And Constant Related Questions

---
+++ 01_variable_and_constants/question.js
@@ -0,0 +1,174 @@
+// 1. Declare a variable and assign your name to it.
+
+/*
+let myName = "Harry";
+*/
+
+
+// 2. Declare two variables and print their sum.
+
+/*
+let num1 = 20;
+let num2 = 10;
+console.log(`Addition of ${num1} and ${num2} is = ${num1 + num2}`);
+*/
+
+
+// 3. Swap two variables using a temporary variable.
+
+/*
+let a = 10;
+let b = 20;
+console.log(`A = ${a}`);
+console.log(`B = ${b}`);
+let temp = a;
+a = b;
+b = temp;
+console.log(`A = ${a}`);
+console.log(`B = ${b}`);
+*/
+
+
+// 4. Swap two variables without using a temporary variable.
+
+/*
+let a1 = 101;
+let b1 = 201;
+console.log(`A1 = ${a1}`);
+console.log(`B1 = ${b1}`);
+[a1,b1] = [b1,a1]
+console.log(`A1 = ${a1}`);
+console.log(`B1 = ${b1}`);
+*/
+
+
+// 5. Declare a constant PI with value 3.14.
+
+/*
+const pi = 3.14;
+*/
+
+
+// 6. Try to reassign a constant variable.
+
+/*
+const pi_valu = 214;
+pi_valu = 123;
+*/
+
+
+// 7. Declare a variable without assigning a value and print it.
+
+/*
+let isAvailalble;
+console.log(isAvailalble);
+*/
+
+
+// 8. Check the type of a number variable.
+
+/*
+let number = 23;
+console.log(typeof number);
+*/
+
+
+// 9. Check the type of a string variable.
+
+/*
+let schoolName = "Vidyalay";
+console.log(typeof schoolName);
+*/
+
+
+// 10. Declare multiple variables in one line.
+
+/*
+let number1 = 101, number2 = 202;
+console.log(number1);
+console.log(number2);
+*/
+
+
+// 11. Store (10 + 20) * 3 in a variable.
+
+/*
+let result = (10 + 20) * 3
+console.log(result);
+*/
+
+
+// 12. Create a boolean variable.
+
+/*
+let isActive = false;
+console.log(isActive);
+*/
+
+
+// 13. Create a variable and assign null.
+
+/*
+let newVar = null;
+console.log(newVar);
+*/
+
+
+// 14. Create a variable and assign undefined.
+
+/*
+let name = undefined; // bad practice
+console.log(name);
+*/
+
+
+// 15. Store your age and print it.
+
+/*
+let age = 23;
+console.log(`My age is ${age} years old.`);
+*/
+
+
+// 16. Store two strings and concatenate them.
+
+/*
+let first_name = "Anu";
+let last_name = "Pande";
+let full_name = first_name + ' ' + last_name;
+console.log(full_name);
+*/
+
+
+// 17. Assign one variable value to another variable.
+
+/*
+let number1 = 20;
+let number2 = number1;
+console.log(number1);
+console.log(number2);
+*/
+
+
+// 18. Create a variable using var.
+
+/*
+var a = 35;
+console.log(a);
+*/
+
+
+// 19. Create a variable using let.
+
+/*
+let a = 35;
+console.log(a);
+*/
+
+
+// 20. Create a variable using const.
+
+/*
+const pi = 3.14;
+console.log(pi);
+*/
