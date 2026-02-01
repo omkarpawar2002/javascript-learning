7c60671526bd5b75992d2a224d41551da9801d6f
omkar <omkarsp20@gmail.com>
2026-02-01 23:56:01 +0530

    Added for loop

---
+++ 05_control_statements/for_loop.js
@@ -0,0 +1,25 @@
+/*
+Control Statements :- 
+        Control statements are used to execute certain code statements in repetative manner.
+
+        There are mainly 3 types of control statements in javascript :-
+            1.for loop 
+            2.while loop
+            3.do-while loop
+
+    1.for loop :- 
+        When we know the exact number of time the ieration is required then we used a for loop.
+        For loop also used to iterate over an arrays.
+*/
+
+// Suppose i want to print welcome 5 times
+for (let i = 0; i < 5; i++) {
+    console.log("Welcome");
+}
+
+console.log('----------------')
+
+// Suppose i want to print number from 1 to 10
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
