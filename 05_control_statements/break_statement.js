b14a92c0e80ec2f6d1b3bc7cb00016c65262eace
omkar <omkarsp20@gmail.com>
2026-02-01 23:57:52 +0530

    Added Break control statement

---
+++ 05_control_statements/break_statement.js
@@ -0,0 +1,12 @@
+/*
+break statements :- 
+        break statements are used to immediatly terminate the block of code.
+        break keyword is used to exits from the loops.
+*/
+
+for (let i = 0; i < 11; i++) {
+    if (i == 6) {
+        break;
+    }
+    console.log(i);
+}
