e77eeb70f1ee7fab15f6fff550431c99fe8f3078
omkar <omkarsp20@gmail.com>
2026-02-01 23:56:54 +0530

    Added do while loop

---
+++ 05_control_statements/do_while_loop.js
@@ -0,0 +1,12 @@
+/*
+3.do while loop :- 
+        do-while loop executed 1 time then it check the condition.
+*/
+
+
+// Suppose i want to print the value from 5 to 1
+let num = 5;
+do{
+    console.log(num);
+    num--;
+}while(num > 0);
