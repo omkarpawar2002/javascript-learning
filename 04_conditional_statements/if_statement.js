0cc9052df5398fdcf9c3d22c079f8c087cfd31c9
omkar <omkarsp20@gmail.com>
2026-02-01 23:52:09 +0530

    Added If statements

---
+++ 04_conditional_statements/if_statement.js
@@ -0,0 +1,22 @@
+/*
+Conditional Statements :- 
+        Conditional statements are used to manipulate the flow of program execution.
+        Sometime we want depending on particular condition the code will get executed at that time we used condition statements.
+
+        Condition statements are 4 types :- 
+            1.If statements
+            2.If else statements
+            3.If - else if .... statements
+            4.If - else if ... - else statements
+
+        
+        1.if statements :- 
+            The if block will get executed only the condition specified after the if keyword is true otherwise it will be skipped.
+*/
+
+let age = 23;
+if(age >= 18){
+    console.log(`You can easily vote because your age is ${age}`);
+}
+
+console.log("Next statement");
