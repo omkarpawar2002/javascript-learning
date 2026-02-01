5ee50b48763933219c46129868b0fa615b6d1485
omkar <omkarsp20@gmail.com>
2026-02-01 23:53:04 +0530

    Added If else statements

---
+++ 04_conditional_statements/if_else_statement.js
@@ -0,0 +1,11 @@
+/*
+2.if else statements:-  
+        The condition specified after the if keyword is true then the if block will get executed and if the condition specified after the if keyword is not true then it will be skipped and else will get executed.
+*/
+
+let age = 3;
+if (age >= 18) {
+    console.log(`You can easily vote because your age is ${age}`);
+} else {
+    console.log(`You can not easily vote because your age is ${age}`);
+}
