a3a5fbe5407c7c553c6a7e42a41bc1c95585cbb1
omkar <omkarsp20@gmail.com>
2026-02-10 23:37:29 +0530

    Initial Commit : Added String Data Structure

---
+++ 07_strings/strings.js
@@ -0,0 +1,21 @@
+/*
+    strings :- 
+        1.A string is a sequence of characters.
+        2.Anything which kept together inside single quotes , double qoutes and backticks are treated as a string.
+*/
+
+
+// Here are the different ways to write a string
+
+// Using a single quotes
+let name = 'Ashu';
+console.log(name);
+
+// Using a Double quotes
+let name1 = "Vikrant";
+console.log(name1);
+
+// Using a Backticks
+let name2 = `Nirmala`;
+console.log(name2);
+
